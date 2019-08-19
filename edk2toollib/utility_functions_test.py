053cbb1873edd94fd1340f728f2d8b40a74b2932
Matthew Carlson <macarl@microsoft.com>
2019-08-19 11:28:55 -0700

    fleshed out tests

--- edk2toollib/utility_functions_test.py
+++ edk2toollib/utility_functions_test.py
@@ -1,5 +1,5 @@
 # @file utility_functions_test.py
-# unit test for locate_tools module.  Only runs on Windows machines.
+# unit test for utility_functions module.
 #
 #
 # Copyright (c) Microsoft Corporation
@@ -15,10 +15,25 @@ import edk2toollib.utility_functions as utilities
 
 class UtilityFunctionsTest(unittest.TestCase):
 
-    def test_RunPythonScript(self): # simple run yourself!
+    def test_RunPythonScript(self):  # simple run yourself!
         path = __file__
         working_dir = os.path.dirname(__file__)
-        utilities.RunPythonScript(path, "", working_dir)
+        ret = utilities.RunPythonScript(path, "", working_dir)
+        self.assertEqual(ret, 0)
+        # test it with all the named parameters
+        ret = utilities.RunPythonScript(path, "", capture=False, workingdir=working_dir,
+                                        raise_exception_on_nonzero=True)
+        self.assertEqual(ret, 0)
+        # now try a bad path
+        bad_path = __file__ + ".super.bad"
+        ret = utilities.RunPythonScript(bad_path, "", capture=False, workingdir=None, raise_exception_on_nonzero=False)
+        self.assertNotEqual(ret, 0)
+        # now we expect it to throw an exception
+        with self.assertRaises(Exception):
+            ret = utilities.RunPythonScript(bad_path, "", capture=False, workingdir=None,
+                                            raise_exception_on_nonzero=True)
+            self.assertNotEqual(ret, 0)
+
 
 # DO NOT PUT A MAIN FUNCTION HERE
 # this test runs itself to test runpython script, which is a tad bit strange yes.
