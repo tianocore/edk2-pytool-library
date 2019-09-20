750446595d0ef380a5237f0648ca3c139d029db6
matthewfcarlson <matthewfcarlson@users.noreply.github.com>
2019-09-19 18:42:02 -0600

    upper case for intel

--- edk2toollib/utility_functions.py
+++ edk2toollib/utility_functions.py
@@ -103,7 +103,7 @@ def GetHostInfo():
     arch = None
     bit = None
 
-    if ("x86" in processor_info.lower()) or ("AMD" in processor_info.upper()) or ("Intel" in processor_info):
+    if ("x86" in processor_info.lower()) or ("AMD" in processor_info.upper()) or ("INTEL" in processor_info.upper()):
         arch = "x86"
     elif ("ARM" in processor_info.upper()) or ("AARCH" in processor_info.upper()):
         arch = "ARM"
