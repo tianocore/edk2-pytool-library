591ebb4cd01dcb59876159abf25476d47f55c4cc
Bret Barkelew <brbarkel@microsoft.com>
2019-10-10 10:54:21 -0700

    Resolve flake errors in files.

--- edk2toollib/uefi/fmp_auth_header.py
+++ edk2toollib/uefi/fmp_auth_header.py
@@ -13,6 +13,7 @@ FmpAuthHeader
 import struct
 import uuid
 
+
 class FmpAuthHeaderClass (object):
     # ///
     # /// Image Attribute -Authentication Required
@@ -96,58 +97,56 @@ class FmpAuthHeaderClass (object):
     #   }
 
     _StructFormat = '<QIHH16s'
-    _StructSize   = struct.calcsize (_StructFormat)
+    _StructSize = struct.calcsize(_StructFormat)
 
     _MonotonicCountFormat = '<Q'
-    _MonotonicCountSize   = struct.calcsize (_MonotonicCountFormat)
+    _MonotonicCountSize = struct.calcsize(_MonotonicCountFormat)
 
     _StructAuthInfoFormat = '<IHH16s'
-    _StructAuthInfoSize   = struct.calcsize (_StructAuthInfoFormat)
+    _StructAuthInfoSize = struct.calcsize(_StructAuthInfoFormat)
 
-    _WIN_CERT_REVISION        = 0x0200
-    _WIN_CERT_TYPE_EFI_GUID   = 0x0EF1
-    _EFI_CERT_TYPE_PKCS7_GUID = uuid.UUID ('4aafd29d-68df-49ee-8aa9-347d375665a7')
+    _WIN_CERT_REVISION = 0x0200
+    _WIN_CERT_TYPE_EFI_GUID = 0x0EF1
+    _EFI_CERT_TYPE_PKCS7_GUID = uuid.UUID('4aafd29d-68df-49ee-8aa9-347d375665a7')
 
-    def __init__ (self):
-        self._Valid              = False
-        self.MonotonicCount      = 0
-        self.dwLength            = self._StructAuthInfoSize
-        self.wRevision           = self._WIN_CERT_REVISION
-        self.wCertificateType    = self._WIN_CERT_TYPE_EFI_GUID
-        self.CertType            = self._EFI_CERT_TYPE_PKCS7_GUID
-        self.CertData            = b''
-        self.Payload             = b''
+    def __init__(self):
+        self._Valid = False
+        self.MonotonicCount = 0
+        self.dwLength = self._StructAuthInfoSize
+        self.wRevision = self._WIN_CERT_REVISION
+        self.wCertificateType = self._WIN_CERT_TYPE_EFI_GUID
+        self.CertType = self._EFI_CERT_TYPE_PKCS7_GUID
+        self.CertData = b''
+        self.Payload = b''
 
-
-    def Encode (self):
+    def Encode(self):
         if self.wRevision != self._WIN_CERT_REVISION:
             raise ValueError
         if self.wCertificateType != self._WIN_CERT_TYPE_EFI_GUID:
             raise ValueError
         if self.CertType != self._EFI_CERT_TYPE_PKCS7_GUID:
             raise ValueError
-        self.dwLength = self._StructAuthInfoSize + len (self.CertData)
+        self.dwLength = self._StructAuthInfoSize + len(self.CertData)
 
-        FmpAuthHeader = struct.pack (
-                                 self._StructFormat,
-                                 self.MonotonicCount,
-                                 self.dwLength,
-                                 self.wRevision,
-                                 self.wCertificateType,
-                                 self.CertType.bytes_le
-                                 )
+        FmpAuthHeader = struct.pack(
+            self._StructFormat,
+            self.MonotonicCount,
+            self.dwLength,
+            self.wRevision,
+            self.wCertificateType,
+            self.CertType.bytes_le
+        )
         self._Valid = True
 
         return FmpAuthHeader + self.CertData + self.Payload
 
-    def Decode (self, Buffer):
-        if len (Buffer) < self._StructSize:
+    def Decode(self, Buffer):
+        if len(Buffer) < self._StructSize:
             raise ValueError
-        (MonotonicCount, dwLength, wRevision, wCertificateType, CertType) = \
-            struct.unpack (
-                     self._StructFormat,
-                     Buffer[0:self._StructSize]
-                     )
+        (MonotonicCount, dwLength, wRevision, wCertificateType, CertType) = struct.unpack(
+            self._StructFormat,
+            Buffer[0:self._StructSize]
+        )
         if dwLength < self._StructAuthInfoSize:
             raise ValueError
         if wRevision != self._WIN_CERT_REVISION:
@@ -156,35 +155,41 @@ class FmpAuthHeaderClass (object):
             raise ValueError
         if CertType != self._EFI_CERT_TYPE_PKCS7_GUID.bytes_le:
             raise ValueError
-        self.MonotonicCount   = MonotonicCount
-        self.dwLength         = dwLength
-        self.wRevision        = wRevision
+        self.MonotonicCount = MonotonicCount
+        self.dwLength = dwLength
+        self.wRevision = wRevision
         self.wCertificateType = wCertificateType
-        self.CertType         = uuid.UUID (bytes_le = CertType)
-        self.CertData         = Buffer[self._StructSize:self._MonotonicCountSize + self.dwLength]
-        self.Payload          = Buffer[self._MonotonicCountSize + self.dwLength:]
-        self._Valid           = True
+        self.CertType = uuid.UUID(bytes_le=CertType)
+        self.CertData = Buffer[self._StructSize:self._MonotonicCountSize + self.dwLength]
+        self.Payload = Buffer[self._MonotonicCountSize + self.dwLength:]
+        self._Valid = True
         return self.Payload
 
-    def IsSigned (self, Buffer):
-        if len (Buffer) < self._StructSize:
+    def IsSigned(self, Buffer):
+        if len(Buffer) < self._StructSize:
             return False
-        (MonotonicCount, dwLength, wRevision, wCertificateType, CertType) = \
-            struct.unpack (
-                     self._StructFormat,
-                     Buffer[0:self._StructSize]
-                     )
+        (MonotonicCount, dwLength, wRevision, wCertificateType, CertType) = struct.unpack(
+            self._StructFormat,
+            Buffer[0:self._StructSize]
+        )
         if CertType != self._EFI_CERT_TYPE_PKCS7_GUID.bytes_le:
             return False
         return True
 
-    def DumpInfo (self):
+    def DumpInfo(self):
         if not self._Valid:
             raise ValueError
-        print ('EFI_FIRMWARE_IMAGE_AUTHENTICATION.MonotonicCount                = {MonotonicCount:016X}'.format (MonotonicCount = self.MonotonicCount))
-        print ('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.Hdr.dwLength         = {dwLength:08X}'.format (dwLength = self.dwLength))
-        print ('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.Hdr.wRevision        = {wRevision:04X}'.format (wRevision = self.wRevision))
-        print ('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.Hdr.wCertificateType = {wCertificateType:04X}'.format (wCertificateType = self.wCertificateType))
-        print ('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.CertType             = {Guid}'.format (Guid = str(self.CertType).upper()))
-        print ('sizeof (EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.CertData)    = {Size:08X}'.format (Size = len (self.CertData)))
-        print ('sizeof (Payload)                                                = {Size:08X}'.format (Size = len (self.Payload)))
+        print('EFI_FIRMWARE_IMAGE_AUTHENTICATION.MonotonicCount                = {MonotonicCount:016X}'
+              .format(MonotonicCount=self.MonotonicCount))
+        print('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.Hdr.dwLength         = {dwLength:08X}'
+              .format(dwLength=self.dwLength))
+        print('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.Hdr.wRevision        = {wRevision:04X}'
+              .format(wRevision=self.wRevision))
+        print('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.Hdr.wCertificateType = {wCertificateType:04X}'
+              .format(wCertificateType=self.wCertificateType))
+        print('EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.CertType             = {Guid}'
+              .format(Guid=str(self.CertType).upper()))
+        print('sizeof (EFI_FIRMWARE_IMAGE_AUTHENTICATION.AuthInfo.CertData)    = {Size:08X}'
+              .format(Size=len(self.CertData)))
+        print('sizeof (Payload)                                                = {Size:08X}'
+              .format(Size=len(self.Payload)))
--- edk2toollib/uefi/uefi_capsule_header.py
+++ edk2toollib/uefi/uefi_capsule_header.py
@@ -12,6 +12,7 @@ UefiCapsuleHeader
 import struct
 import uuid
 
+
 class UefiCapsuleHeaderClass (object):
     # typedef struct {
     #   ///
@@ -41,26 +42,26 @@ class UefiCapsuleHeaderClass (object):
     # #define CAPSULE_FLAGS_INITIATE_RESET                0x00040000
     #
     _StructFormat = '<16sIIII'
-    _StructSize   = struct.calcsize (_StructFormat)
+    _StructSize = struct.calcsize(_StructFormat)
 
-    EFI_FIRMWARE_MANAGEMENT_CAPSULE_ID_GUID = uuid.UUID ('6DCBD5ED-E82D-4C44-BDA1-7194199AD92A')
+    EFI_FIRMWARE_MANAGEMENT_CAPSULE_ID_GUID = uuid.UUID('6DCBD5ED-E82D-4C44-BDA1-7194199AD92A')
 
-    _CAPSULE_FLAGS_PERSIST_ACROSS_RESET  = 0x00010000
+    _CAPSULE_FLAGS_PERSIST_ACROSS_RESET = 0x00010000
     _CAPSULE_FLAGS_POPULATE_SYSTEM_TABLE = 0x00020000
-    _CAPSULE_FLAGS_INITIATE_RESET        = 0x00040000
+    _CAPSULE_FLAGS_INITIATE_RESET = 0x00040000
 
-    def __init__ (self):
-        self._Valid              = False
-        self.CapsuleGuid         = self.EFI_FIRMWARE_MANAGEMENT_CAPSULE_ID_GUID
-        self.HeaderSize          = self._StructSize
-        self.OemFlags            = 0x0000
-        self.PersistAcrossReset  = False
+    def __init__(self):
+        self._Valid = False
+        self.CapsuleGuid = self.EFI_FIRMWARE_MANAGEMENT_CAPSULE_ID_GUID
+        self.HeaderSize = self._StructSize
+        self.OemFlags = 0x0000
+        self.PersistAcrossReset = False
         self.PopulateSystemTable = False
-        self.InitiateReset       = False
-        self.CapsuleImageSize    = self.HeaderSize
-        self.Payload             = b''
+        self.InitiateReset = False
+        self.CapsuleImageSize = self.HeaderSize
+        self.Payload = b''
 
-    def Encode (self):
+    def Encode(self):
         Flags = self.OemFlags
         if self.PersistAcrossReset:
             Flags = Flags | self._CAPSULE_FLAGS_PERSIST_ACROSS_RESET
@@ -69,44 +70,43 @@ class UefiCapsuleHeaderClass (object):
         if self.InitiateReset:
             Flags = Flags | self._CAPSULE_FLAGS_INITIATE_RESET
 
-        self.CapsuleImageSize = self.HeaderSize + len (self.Payload)
+        self.CapsuleImageSize = self.HeaderSize + len(self.Payload)
 
-        UefiCapsuleHeader = struct.pack (
-                                     self._StructFormat,
-                                     self.CapsuleGuid.bytes_le,
-                                     self.HeaderSize,
-                                     Flags,
-                                     self.CapsuleImageSize,
-                                     0
-                                     )
+        UefiCapsuleHeader = struct.pack(
+            self._StructFormat,
+            self.CapsuleGuid.bytes_le,
+            self.HeaderSize,
+            Flags,
+            self.CapsuleImageSize,
+            0
+        )
         self._Valid = True
         return UefiCapsuleHeader + self.Payload
 
-    def Decode (self, Buffer):
-        if len (Buffer) < self._StructSize:
+    def Decode(self, Buffer):
+        if len(Buffer) < self._StructSize:
             raise ValueError
-        (CapsuleGuid, HeaderSize, Flags, CapsuleImageSize, Reserved) = \
-            struct.unpack (
-                     self._StructFormat,
-                     Buffer[0:self._StructSize]
-                     )
+        (CapsuleGuid, HeaderSize, Flags, CapsuleImageSize, Reserved) = struct.unpack(
+            self._StructFormat,
+            Buffer[0:self._StructSize]
+        )
         if HeaderSize < self._StructSize:
             raise ValueError
-        if CapsuleImageSize != len (Buffer):
+        if CapsuleImageSize != len(Buffer):
             raise ValueError
-        self.CapsuleGuid         = uuid.UUID (bytes_le = CapsuleGuid)
-        self.HeaderSize          = HeaderSize
-        self.OemFlags            = Flags & 0xffff
-        self.PersistAcrossReset  = (Flags & self._CAPSULE_FLAGS_PERSIST_ACROSS_RESET) != 0
+        self.CapsuleGuid = uuid.UUID(bytes_le=CapsuleGuid)
+        self.HeaderSize = HeaderSize
+        self.OemFlags = Flags & 0xffff
+        self.PersistAcrossReset = (Flags & self._CAPSULE_FLAGS_PERSIST_ACROSS_RESET) != 0
         self.PopulateSystemTable = (Flags & self._CAPSULE_FLAGS_POPULATE_SYSTEM_TABLE) != 0
-        self.InitiateReset       = (Flags & self._CAPSULE_FLAGS_INITIATE_RESET) != 0
-        self.CapsuleImageSize    = CapsuleImageSize
-        self.Payload             = Buffer[self.HeaderSize:]
+        self.InitiateReset = (Flags & self._CAPSULE_FLAGS_INITIATE_RESET) != 0
+        self.CapsuleImageSize = CapsuleImageSize
+        self.Payload = Buffer[self.HeaderSize:]
 
-        self._Valid              = True
+        self._Valid = True
         return self.Payload
 
-    def DumpInfo (self):
+    def DumpInfo(self):
         if not self._Valid:
             raise ValueError
         Flags = self.OemFlags
@@ -116,15 +116,15 @@ class UefiCapsuleHeaderClass (object):
             Flags = Flags | self._CAPSULE_FLAGS_POPULATE_SYSTEM_TABLE
         if self.InitiateReset:
             Flags = Flags | self._CAPSULE_FLAGS_INITIATE_RESET
-        print ('EFI_CAPSULE_HEADER.CapsuleGuid      = {Guid}'.format (Guid = str(self.CapsuleGuid).upper()))
-        print ('EFI_CAPSULE_HEADER.HeaderSize       = {Size:08X}'.format (Size = self.HeaderSize))
-        print ('EFI_CAPSULE_HEADER.Flags            = {Flags:08X}'.format (Flags = Flags))
-        print ('  OEM Flags                         = {Flags:04X}'.format (Flags = self.OemFlags))
+        print('EFI_CAPSULE_HEADER.CapsuleGuid      = {Guid}'.format(Guid=str(self.CapsuleGuid).upper()))
+        print('EFI_CAPSULE_HEADER.HeaderSize       = {Size:08X}'.format(Size=self.HeaderSize))
+        print('EFI_CAPSULE_HEADER.Flags            = {Flags:08X}'.format(Flags=Flags))
+        print('  OEM Flags                         = {Flags:04X}'.format(Flags=self.OemFlags))
         if self.PersistAcrossReset:
-            print ('  CAPSULE_FLAGS_PERSIST_ACROSS_RESET')
+            print('  CAPSULE_FLAGS_PERSIST_ACROSS_RESET')
         if self.PopulateSystemTable:
-            print ('  CAPSULE_FLAGS_POPULATE_SYSTEM_TABLE')
+            print('  CAPSULE_FLAGS_POPULATE_SYSTEM_TABLE')
         if self.InitiateReset:
-            print ('  CAPSULE_FLAGS_INITIATE_RESET')
-        print ('EFI_CAPSULE_HEADER.CapsuleImageSize = {Size:08X}'.format (Size = self.CapsuleImageSize))
-        print ('sizeof (Payload)                    = {Size:08X}'.format (Size = len (self.Payload)))
+            print('  CAPSULE_FLAGS_INITIATE_RESET')
+        print('EFI_CAPSULE_HEADER.CapsuleImageSize = {Size:08X}'.format(Size=self.CapsuleImageSize))
+        print('sizeof (Payload)                    = {Size:08X}'.format(Size=len(self.Payload)))
