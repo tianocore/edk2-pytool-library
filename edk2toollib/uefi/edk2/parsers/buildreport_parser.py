d18a2c591f5b5a12b1f0b01458fcf11be5e34964
Sean Brogan <sean.brogan@microsoft.com>
2020-05-12 01:07:40 -0700

    Change RegionTypes to use Python native Enum type

--- edk2toollib/uefi/edk2/parsers/buildreport_parser.py
+++ edk2toollib/uefi/edk2/parsers/buildreport_parser.py
@@ -7,8 +7,8 @@
 ##
 import os
 import logging
+from enum import Enum
 
-import edk2toollib.utility_functions as uf
 import edk2toollib.uefi.edk2.path_utilities as pu
 
 #
@@ -161,7 +161,12 @@ class ModuleSummary(object):
 
 
 class BuildReport(object):
-    RegionTypes = uf.Enum(['PCD', 'FD', 'MODULE', 'UNKNOWN'])
+
+    class RegionTypes(Enum):
+        PCD = 'PCD'
+        FD = 'FD'
+        MODULE = 'MODULE'
+        UNKNOWN = 'UNKNOWN'
 
     def __init__(self, filepath, ws, packagepathcsv, protectedWordsDict):
         self.PlatformName = ""
@@ -214,7 +219,7 @@ class BuildReport(object):
             type = self._GetRegionType(start)
             self._Regions.append((type, start, end))
             linenum = self._GetNextRegionStart(linenum)
-            logging.debug("Found a region of type: %s start: %d end: %d" % (BuildReport.RegionTypes[type], start, end))
+            logging.debug("Found a region of type: %s start: %d end: %d" % (type, start, end))
 
         #
         # Parse the basic header of the report.
