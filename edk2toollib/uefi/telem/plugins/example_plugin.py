7156dfa3b566b5f05fc3954aab02ee2cf1ab4cff
Taylor Beebe <tabeebe@microsoft.com>
2020-08-13 15:01:21 -0700

    removed test file. updated example plugin

--- edk2toollib/uefi/telem/plugins/example_plugin.py
+++ edk2toollib/uefi/telem/plugins/example_plugin.py
@@ -1,28 +1,37 @@
-# @file cper_section_data.py
-# Base class for all parsing types
+# @file a_test_plugin.py
+# A testing plugin used to verify parsing
 #
 # Copyright (c) Microsoft Corporation
 #
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
 
-class SECTION_PARSER_PLUGIN(object):
+import sys
+import uuid
 
+sys.path.append('../..')
+from cper_section_data import SECTION_PARSER_PLUGIN
+
+class EXAMPLE_PLUGIN(SECTION_PARSER_PLUGIN):
+
+    def __init__(self):
+        pass
+    
     ##
     # Returns string representation of this parser
     ##
     def __str__(self) -> str:
-        raise NotImplementedError
+        return "EXAMPLE PLUGIN"
 
     ##
     # True if this parser recognizes the input guid
     ##
     def CanParse(self,guid) -> bool:
-        raise NotImplementedError
+        return False
 
     ##
     # Returns a string representation of the data passed in. It is assumed that
     # if this call runs, you are recieving section data you recognize
     ##
     def Parse(self,data) -> str:
-        raise NotImplementedError
+        return ""
