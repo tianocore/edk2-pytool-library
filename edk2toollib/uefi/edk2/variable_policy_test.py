76e58321b7bf1cb90096f83c34a600cac60fe9a0
brbarkel@microsoft.com <brbarkel@microsoft.com>
2021-07-05 14:20:59 -0700

    Add almost full testing for VariablePolicyEntry

--- edk2toollib/uefi/edk2/variable_policy_test.py
+++ edk2toollib/uefi/edk2/variable_policy_test.py
@@ -8,11 +8,14 @@
 
 import unittest
 import uuid
-from edk2toollib.uefi.edk2.variable_policy import VariableLockOnVarStatePolicy
+from edk2toollib.uefi.edk2.variable_policy import VariableLockOnVarStatePolicy, VariablePolicyEntry
 
 
 TEST_GUID_1 = uuid.UUID("48B5F961-3F7D-4B88-9BEE-D305ED8256DA")
 TEST_GUID_2 = uuid.UUID("65D16747-FCBC-4FAE-A727-7B679A7B23F9")
+
+TEST_POLICY_ENTRY = b''.fromhex("000001006A004600E222FFB0EA4A2547A6E55317FB8FD39C00000000FFFFFFFF000000000000000003AFAFAFC690F5ECF9F887438422486E3CCD8B2001AF45004F00440000004C0061007300740041007400740065006D00700074005300740061007400750073000000")
+TEST_POLICY_ENTRY_GUID = uuid.UUID("B0FF22E2-4AEA-4725-A6E5-5317FB8FD39C")
 
 
 class TestVariableLockOnVarStatePolicy(unittest.TestCase):
@@ -60,3 +63,38 @@ class TestVariableLockOnVarStatePolicy(unittest.TestCase):
         test_vpl.decode(test_buffer)
 
         self.assertEqual(test_vpl.Name, "ABC")
+
+
+class TestVariablePolicyEntry(unittest.TestCase):
+    def test_create_and_to_string(self):
+        test_vp = VariablePolicyEntry()
+        to_string = str(test_vp)
+
+        # Check for the LockType string.
+        self.assertIn("NONE", to_string)
+
+        test_vp.LockPolicyType = VariablePolicyEntry.TYPE_LOCK_ON_CREATE
+        to_string = str(test_vp)
+
+        # Check for the new LockType string.
+        self.assertIn("CREATE", to_string)
+
+    def test_csv_formatting(self):
+        header_row = VariablePolicyEntry.csv_header()
+        self.assertIn("Namespace", header_row)
+        self.assertIn("LockPolicyType", header_row)
+
+        test_vp = VariablePolicyEntry()
+        test_vp.LockPolicyType = VariablePolicyEntry.TYPE_LOCK_ON_CREATE
+        csv_row = test_vp.csv_row()
+        self.assertEqual(len(header_row), len(csv_row))
+        self.assertIn("ON_CREATE", csv_row)
+
+    def test_decoding(self):
+        test_vp = VariablePolicyEntry()
+        test_vp.decode(TEST_POLICY_ENTRY)
+
+        self.assertEqual(test_vp.Namespace, TEST_POLICY_ENTRY_GUID)
+        self.assertEqual(test_vp.LockPolicyType, VariablePolicyEntry.TYPE_LOCK_ON_VAR_STATE)
+        self.assertEqual(test_vp.Name, "LastAttemptStatus")
+        self.assertEqual(test_vp.LockPolicy.Name, "EOD")
