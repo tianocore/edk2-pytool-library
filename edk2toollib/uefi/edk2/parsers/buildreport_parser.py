0eac598e871e8f74068ac00089bf39f09d1264eb
Edwin Zhang <edwinz@microsoft.com>
2021-09-10 12:34:16 -0700

    Add workaround in buildreport_parser.py to handle dynamic PCDs for projects with multiple SKUs

--- edk2toollib/uefi/edk2/parsers/buildreport_parser.py
+++ edk2toollib/uefi/edk2/parsers/buildreport_parser.py
@@ -115,7 +115,10 @@ class ModuleSummary(object):
                                 line += " " + self._RawContent[i].rstrip()
 
                             token = line.partition('=')[2]
-                            token2 = line.partition(':')[0].split()[-1]
+                            if(line.partition(':')[0].split() == []):
+                                token2 = ""
+                            else:
+                                token2 = line.partition(':')[0].split()[-1]
                             self.PCDs[tokenspace + "." + token2] = token.strip()
 
                         # this is the secondary lines of PCD values showing Defaults
