bfc9a76b4c9b351b3f999220e0c9c219335bd4f2
Joey Vagedes <joeyvagedes@microsoft.com>
2022-04-12 16:39:06 -0700

    Revert "Updated how to detect nested packages"
    This reverts commit 6a8771189c6c460da9b8e922d509e3f5b9347e0f.

--- edk2toollib/uefi/edk2/path_utilities.py
+++ edk2toollib/uefi/edk2/path_utilities.py
@@ -65,15 +65,26 @@ class Edk2Path(object):
         if(error and error_on_invalid_pp):
             raise Exception("Invalid package path directory(s)")
 
-        # Iterate through all package paths to see if the path is inside another package. This
-        # indicates nested packages.
-        allowed_paths = [os.path.basename(packagepath).lower() for packagepath in self.PackagePathList]
-        allowed_paths.append(os.path.basename(self.WorkspacePath).lower())
+        # for each package path, trace from packagepath to the
+        # workspace root and verify no *.DEC file exists.
+        # This would signify a nested package
         for package_path in self.PackagePathList:
-            containing_package = self.GetContainingPackage(package_path)
+            p = Path(package_path)
+            ws = self.WorkspacePath
 
-            if containing_package is not None and containing_package.lower() not in allowed_paths:
-                raise Exception(f'Package path [{package_path}] is nested inside of the package {containing_package}')
+            while ws in str(p):
+                if len(glob.glob(f'{p}/*dec')) != 0:
+                    logging.error(
+                        f' Detected Nested Packages. {package_path} is a package path, however a package DEC file was found \
+                            at: {p}')
+                    raise Exception(
+                        f' Detected Nested Packages. {package_path} is a package path, however a package DEC file was found \
+                            at: {p}')
+
+                # Catch a situation where the workspace path is the root of a filesystem (D:\, etc)
+                if p == p.parent:
+                    break
+                p = p.parent
 
     def GetEdk2RelativePathFromAbsolutePath(self, abspath):
         ''' Given an absolute path return a edk2 path relative
