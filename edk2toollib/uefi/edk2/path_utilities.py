a98108b3e2fec4f6ca1d2bc03f254b4877a3f965
Michael Kubacki <michael.kubacki@microsoft.com>
2022-11-04 09:06:40 -0400

    PR Update: Add back PR #113 support
    Allows callers to pass package paths that may not be valid
    directories and control whether an exception is raised with the
    error_on_invalid_pp Edk2Path constructor parameter.
    
    Restores behavior introduced in https://github.com/tianocore/edk2-pytool-library/pull/113
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- edk2toollib/uefi/edk2/path_utilities.py
+++ edk2toollib/uefi/edk2/path_utilities.py
@@ -69,17 +69,20 @@ class Edk2Path(object):
                     # assume current working dir relative.  Will catch invalid dir when checking whole list
                     candidate_package_path_list.append(Path(os.getcwd(), a))
 
-        for a in candidate_package_path_list:
+        error = False
+        for a in candidate_package_path_list[:]:
             if not a.is_dir():
-                if error_on_invalid_pp:
-                    raise NotADirectoryError(
-                        errno.ENOENT,
-                        os.strerror(errno.ENOENT),
-                        a.resolve())
-                else:
-                    return
+                self.logger.log(logging.ERROR if error_on_invalid_pp else
+                                logging.WARNING,
+                                f"Invalid package path entry {a.resolve()}")
+                candidate_package_path_list.remove(a)
+                error = True
 
         self.PackagePathList = [str(p) for p in candidate_package_path_list]
+
+        if error and error_on_invalid_pp:
+            raise NotADirectoryError(errno.ENOENT, os.strerror(errno.ENOENT),
+                                     a.resolve())
 
         # Nested package check - ensure packages do not exist in a linear
         # path hierarchy.
