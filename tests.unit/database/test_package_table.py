7bb22dcb75caa0de551a235e136712a9e00ad6ef
Joey Vagedes <joeyvagedes@microsoft.com>
2023-09-18 16:08:54 -0700

    update

--- tests.unit/database/test_package_table.py
+++ tests.unit/database/test_package_table.py
@@ -6,24 +6,22 @@
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
 """Tests for building a package table."""
-import sys
 
 import git
-import pytest
 from edk2toollib.database import Edk2DB
 from edk2toollib.database.tables import PackageTable
 from edk2toollib.uefi.edk2.path_utilities import Edk2Path
 
 
-@pytest.mark.skipif(sys.platform.startswith("win"), reason="Linux only")
+#@pytest.mark.skipif(sys.platform.startswith("win"), reason="Linux only")
 def test_basic_parse(tmp_path):
     """Tests basic PackageTable functionality."""
     # Clone the repo and init a single submodule.
     repo_path = tmp_path / "mu_tiano_platforms"
-    repo = git.Repo.clone_from("https://github.com/microsoft/mu_tiano_platforms", repo_path)
-    if repo is None:
-        raise Exception("Failed to clone mu_tiano_platforms")
-    repo.git.submodule("update", "--init", "Features/CONFIG")
+    with git.Repo.clone_from("https://github.com/microsoft/mu_tiano_platforms", repo_path) as repo:
+        if repo is None:
+            raise Exception("Failed to clone mu_tiano_platforms")
+        repo.git.submodule("update", "--init", "Features/CONFIG")
 
     edk2path = Edk2Path(str(repo_path), ["Platforms", "Features/CONFIG"])
     db = Edk2DB(tmp_path / "db.db", pathobj=edk2path)
