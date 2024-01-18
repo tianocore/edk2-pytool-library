b094f9f792145d2c2ed6fbe9d2e5f3212d54236a
Joey Vagedes <joey.vagedes@gmail.com>
2024-01-18 08:04:44 -0800

    use absolute import for tables

--- edk2toollib/database/tables/__init__.py
+++ edk2toollib/database/tables/__init__.py
@@ -5,10 +5,9 @@
 ##
 """A collection of table generators that run against the workspace."""
 from edk2toollib.database.edk2_db import TableGenerator  # noqa: F401
-
-from .environment_table import EnvironmentTable  # noqa: F401
-from .inf_table import InfTable  # noqa: F401
-from .instanced_fv_table import InstancedFvTable  # noqa: F401
-from .instanced_inf_table import InstancedInfTable  # noqa: F401
-from .package_table import PackageTable  # noqa: F401
-from .source_table import SourceTable  # noqa: F401
+from edk2toollib.database.tables.environment_table import EnvironmentTable  # noqa: F401
+from edk2toollib.database.tables.inf_table import InfTable  # noqa: F401
+from edk2toollib.database.tables.instanced_fv_table import InstancedFvTable  # noqa: F401
+from edk2toollib.database.tables.instanced_inf_table import InstancedInfTable  # noqa: F401
+from edk2toollib.database.tables.package_table import PackageTable  # noqa: F401
+from edk2toollib.database.tables.source_table import SourceTable  # noqa: F401
